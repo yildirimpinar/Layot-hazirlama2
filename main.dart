b1188bc6cbc9cbf5fa2f33be0d95930d34a0940b
Pınar YILDIRIM <72815396+yildirimpinar@users.noreply.github.com>
2022-11-03 21:32:08 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'widget.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          body: ListView(
+    children: [
+      logoSection,
+      loginSection,
+    ],
+  ))));
+}
